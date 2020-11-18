654c664d4a6e5694f1515c765f7c0b062d8740a0
sworkman <steven.workman@logmein.com>
2020-11-17 22:09:01 -0700

    switching back to othe SF org

--- src/server/server.js
+++ src/server/server.js
@@ -3,10 +3,10 @@ const express = require('express'),
     jsforce = require('jsforce'),
     Configuration = require('./utils/configuration.js'),
     WebSocketService = require('./utils/webSocketService.js'),
-    QuizSessionRestResource = require('./rest/quiz-session.js'),
-    PlayerRestResource = require('./rest/player.js'),
-    AnswerRestResource = require('./rest/answer.js'),
-    ConfigurationRestResource = require('./rest/configuration.js');
+    // QuizSessionRestResource = require('./rest/quiz-session.js'),
+    // PlayerRestResource = require('./rest/player.js'),
+    // AnswerRestResource = require('./rest/answer.js'),
+    // ConfigurationRestResource = require('./rest/configuration.js');
 
 // Load and check config
 require('dotenv').config();
@@ -44,41 +44,41 @@ sfdc.login(
     console.log('Connected to Salesforce');
 });
 
-// Setup Quiz Session REST resources
-const quizSessionRest = new QuizSessionRestResource(sfdc, wss);
-app.get('/api/quiz-sessions', (request, response) => {
-    quizSessionRest.getSession(request, response);
-});
-app.put('/api/quiz-sessions', (request, response) => {
-    quizSessionRest.updateSession(request, response);
-});
+// // Setup Quiz Session REST resources
+// const quizSessionRest = new QuizSessionRestResource(sfdc, wss);
+// app.get('/api/quiz-sessions', (request, response) => {
+//     quizSessionRest.getSession(request, response);
+// });
+// app.put('/api/quiz-sessions', (request, response) => {
+//     quizSessionRest.updateSession(request, response);
+// });
 
-// Setup Players REST resources
-const playerRest = new PlayerRestResource(sfdc);
-app.get('/api/players', (request, response) => {
-    playerRest.isNicknameAvailable(request, response);
-});
-app.get('/api/players/:playerId/stats', (request, response) => {
-    playerRest.getPlayerStats(request, response);
-});
-app.get('/api/players/:playerId/leaderboard', (request, response) => {
-    playerRest.getPlayerLeaderboard(request, response);
-});
-app.post('/api/players', (request, response) => {
-    playerRest.registerPlayer(request, response);
-});
+// // Setup Players REST resources
+// const playerRest = new PlayerRestResource(sfdc);
+// app.get('/api/players', (request, response) => {
+//     playerRest.isNicknameAvailable(request, response);
+// });
+// app.get('/api/players/:playerId/stats', (request, response) => {
+//     playerRest.getPlayerStats(request, response);
+// });
+// app.get('/api/players/:playerId/leaderboard', (request, response) => {
+//     playerRest.getPlayerLeaderboard(request, response);
+// });
+// app.post('/api/players', (request, response) => {
+//     playerRest.registerPlayer(request, response);
+// });
 
-// Setup Answer REST resources
-const answerRest = new AnswerRestResource(sfdc);
-app.post('/api/answers', (request, response) => {
-    answerRest.submitAnswer(request, response);
-});
+// // Setup Answer REST resources
+// const answerRest = new AnswerRestResource(sfdc);
+// app.post('/api/answers', (request, response) => {
+//     answerRest.submitAnswer(request, response);
+// });
 
-// Setup Configuration REST resources
-const configurationRest = new ConfigurationRestResource();
-app.get('/api/configuration', (request, response) => {
-    configurationRest.getConfiguration(request, response);
-});
+// // Setup Configuration REST resources
+// const configurationRest = new ConfigurationRestResource();
+// app.get('/api/configuration', (request, response) => {
+//     configurationRest.getConfiguration(request, response);
+// });
 
 // HTTP and WebSocket Listen
 const PORT = process.env.PORT || 3002;
