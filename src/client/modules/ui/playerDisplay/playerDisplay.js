2121dd362b5795ce439d07de82ef7a157a8b39f3
sworkman <steven.workman@logmein.com>
2020-12-09 20:38:29 -0700

    playerDisplay updates

--- src/client/modules/ui/playerDisplay/playerDisplay.js
+++ src/client/modules/ui/playerDisplay/playerDisplay.js
@@ -1,52 +1,25 @@
+// import LightningElementWithSLDS from '../../../lib/lightningElementWithSLDS.js';
 import { LightningElement, track, api } from 'lwc';
 // import FORM_FACTOR  from '@salesforce/client/formFactor'
 
-// import getPlayerInfo from '@salesforce/apex/GameController.getPlayerInfo';
-
+// export default class PlayerDisplay extends LightningElementWithSLDS {
 export default class PlayerDisplay extends LightningElement {
-    @api playerId;
+    // @api playerId;
+    @api player;
     @api cardBack;
     @track cardImage;
-    @track cardDescription;
-    @track cardName;
+    // @track cardDescription;
+    // @track cardName;
     @track showDescription = false;
-
-    @api
-    get playerInfo() {
-        return this._playerInfo;
-    }
-    set playerInfo(value) {
-        this.setAttribute('playerInfo', value);
-        this._playerInfo = value;
-        this.handlePlayerUpdate();
-    }
-    @track _playerInfo;
     formFactor = 'Large';
 
-    @track displayInfo = false;
-
     connectedCallback(){
-        this.cardDescription = 'Waiting for cards to be dealt';
-        this.showDescription = true;
-        this.displayInfo = true;
-    }
-
-    handlePlayerUpdate(){
-        window.console.log('handlePlayerUpdate: playerInfo = ', JSON.stringify(this.playerInfo));
-        if(this.playerInfo != undefined && this.playerInfo.Game__r.Status__c == 'cardsDealt'){
-            window.console.log('we got here');
-            this.displayInfo = false;
-            this.showDescription = false;
-            this.cardImage = this.cardBack;
-            this.cardDescription = this.playerInfo.Original_GameCard__r.Card__r.Description;
-            this.cardName = this.playerInfo.Original_Card_Name__c;
-            this.displayInfo = true;
-        }
+        this.cardImage = this.cardBack;
     }
 
     handleFlipCard(){
-        if(this.playerInfo.Original_GameCard__r.Card__r.URL__c != undefined && this.playerInfo.Original_GameCard__r.Card__r.URL__c != null){
-            this.cardImage = (this.cardImage === this.cardBack) ?  this.playerInfo.Original_GameCard__r.Card__r.URL__c : this.cardBack;
+        if(this.player.herokuOriginalImage != undefined && this.playerInfo.herokuOriginalImage != null){
+            this.cardImage = this.showDescription ?  this.playerInfo.herokuOriginalImage : this.cardBack;
             this.showDescription = !this.showDescription;
         }
     }
